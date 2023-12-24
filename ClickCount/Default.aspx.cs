44abaa6948913633162eb8fd5352016c2a17b1e9
Aryan98 <aryanvermae@mailcom>
2023-12-24 20:11:33 +0530

    test

---
+++ ClickCount/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DummyProject
+{
+    public partial class Page1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                TextBox1.Text = "0";
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int cnt = Convert.ToInt32(TextBox1.Text);
+            cnt += 1;
+            TextBox1.Text = cnt.ToString();
+        }
+    }
+}
